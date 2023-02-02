e3f8f6aa23f6f6783f71a123ef95c7d9c346e953
miles <41658201+milesnp@users.noreply.github.com>
2023-02-02 11:25:19 -0600

    Count slots from start default true
    Adding hotbar slots with this set to false adds fake slots to the hotbar

--- src/main/java/nuparu/tinyinv/config/ServerConfig.java
+++ src/main/java/nuparu/tinyinv/config/ServerConfig.java
@@ -20,7 +20,7 @@ public class ServerConfig {
         hotbarSlots = server.comment("Number of hotbar slots").defineInRange("general.hotbar_slots", 9, 1, 36);
         armorStartID = server.comment("ID of the first armor (non-container) inventory slot)").defineInRange("general.armor_start_id", 36, 1, Integer.MAX_VALUE);
         disableOffhand = server.comment("Should disable the left hand?").define("general.disable_offhand", false);
-        countSlotsFromStart = server.comment("Counts slots by their numerical ID - hotbar has IDs 0-8, the top inventory row 9-17,....").define("general.count_slots_from_start", false);
+        countSlotsFromStart = server.comment("Counts slots by their numerical ID - hotbar has IDs 0-8, the top inventory row 9-17,....").define("general.count_slots_from_start", true);
         excludeCreativeModePlayers = server.comment("Should exclude the players who are in creative mode?").define("general.exclude_creative_mode_players", true);
     }
 }
